2a8aa6499cd48c0e8bba32de4dc29d8fdde21918
nomiku1999 <tranhuyphuong99@gmail.com>
2021-01-04 21:28:35 +0700

    ResetPlace
    Still get bug. I think the problems is prefab transform.position

--- Assets/PlayerEnterResetPoint.cs
+++ Assets/PlayerEnterResetPoint.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SawDamage : MonoBehaviour
+public class PlayerEnterResetPoint : MonoBehaviour
 {
-    //[SerializeField] private float waitingTime = 2f;
- 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player"))
         {
             GameObject resetPlaceObj = GameObject.FindObjectOfType<ResetPlace>().gameObject;
             ResetPlace resetPlaceSc = resetPlaceObj.GetComponent<ResetPlace>();
-            resetPlaceSc.PlayerDie();
+            resetPlaceSc.SetResetPlace(gameObject.transform.position);
+            Debug.Log(gameObject.transform.position);
+            Destroy(gameObject);
         }
-    }   
+    }
 }
